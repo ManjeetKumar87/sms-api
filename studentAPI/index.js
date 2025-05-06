8e48997686cdcc7c24ce983f3215fc5bf9d1b1e2
ManjeetKumar87 <bscs2312376@szabist.pk>
2025-05-06 12:31:34 +0000

    message update

--- studentAPI/index.js
+++ studentAPI/index.js
@@ -16,25 +16,124 @@ app.get( '/',async(req,res)=>{
 
     app.get('/students',async(req,res)=>{
         try{
-            const result = await pool.query('SELECT * FROM student');
+            const result = await pool.query(`SELECT * FROM student`);
             res.json(result.rows);
         }catch(err){
            res.status(500).json({error: 'Internal server error'});
         }
     });
-    pp.get('/gettotalstd',async(req,res)=>{
+    app.get('/gettotalstd',async(req,res)=>{
         try{
-            const result = await pool.query('SELECT count(ID) FROM student');
+            const result = await pool.query(`select count(sid) from student`);
             res.json(result.rows);
         }catch(err){
-           res.status(500).json({error: 'Internal server error'});
+            res.status(500).json({Error:err.message});
         }
     });
 
-    
+    app.get('/emp', async (req, res) => {
+        try {
+            const result = await pool.query(`
+                SELECT employee_id, job_id, start_date, end_date, department_id
+                FROM job_history
+            `);
+            res.json(result.rows);
+        } catch (err) {
+            console.error("Query Error:", err);
+            res.status(500).json({ Error: err.message });
+        }
+    });
+    app.get('/jobs', async (req, res) => {
+        try {
+            const result = await pool.query(`
+                select * from jobs
+            `);
+            res.json(result.rows);
+        } catch (err) {
+            console.error("Query Error:", err);
+            res.status(500).json({ Error: err.message });
+        }
+    });
+
+    app.get('/countemp', async (req, res) => {
+        try {
+            const result = await pool.query(`
+                select count(employee_id) from employees
+            `);
+            res.json(result.rows);
+        } catch (err) {
+            console.error("Query Error:", err);
+            res.status(500).json({ Error: err.message });
+        }
+    });
+    app.get('/countloc', async (req, res) => {
+        try {
+            const result = await pool.query(`
+                select count(location_id) from locations
+            `);
+            res.json(result.rows);
+        } catch (err) {
+            console.error("Query Error:", err);
+            res.status(500).json({ Error: err.message });
+        }
+    });
+
+    app.get('/countcountries', async (req, res) => {
+        try {
+            const result = await pool.query(`
+                select count(country_id) from countries
+            `);
+            res.json(result.rows);
+        } catch (err) {
+            console.error("Query Error:", err);
+            res.status(500).json({ Error: err.message });
+        }
+    });
+
+    app.get('/countloc', async (req, res) => {
+        try {
+            const result = await pool.query(`
+                select count(location_id) from locations
+            `);
+            res.json(result.rows);
+        } catch (err) {
+            console.error("Query Error:", err);
+            res.status(500).json({ Error: err.message });
+        }
+    });
+    app.get('/countregion', async (req, res) => {
+        try {
+            const result = await pool.query(`
+                select count(region_id) from regions
+            `);
+            res.json(result.rows);
+        } catch (err) {
+            console.error("Query Error:", err);
+            res.status(500).json({ Error: err.message });
+        }
+    });
+    app.get('/countdep', async (req, res) => {
+        try {
+            const result = await pool.query(`
+                select count(department_id) from departments
+            `);
+            res.json(result.rows);
+        } catch (err) {
+            console.error("Query Error:", err);
+            res.status(500).json({ Error: err.message });
+        }
+    });
+
+
+
+
+
+   
 
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`connected successfully.....Running on port ${PORT}`);
 });
+
+
